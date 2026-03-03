ae86c667c2da81bdf60b4b38a708a44c1db1fa7f
shashank416327-beep <shashank416327@gmail.com>
2026-03-03 14:05:36 +0530

    UC6: Created separate class to store banner pattern

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,45 +1,24 @@
+class BannerPattern {
+
+    String[] bannerLines = {
+            String.join(" ", " *****  ", " *****  ", " ****** ", " *****  "),
+            String.join(" ", "*     * ", "*     * ", "*     * ", "*       "),
+            String.join(" ", "*     * ", "*     * ", "******  ", " *****  "),
+            String.join(" ", "*     * ", "*     * ", "*       ", "      * "),
+            String.join(" ", " *****  ", " *****  ", "*       ", " *****  ")
+    };
+
+    void display() {
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
+    }
+}
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        printBanner();
-    }
-
-    public static void printBanner() {
-
-        String[] banner = new String[5];
-
-        banner[0] = String.join(" ",
-                " *****  ",
-                " *****  ",
-                " ****** ",
-                " *****  ");
-
-        banner[1] = String.join(" ",
-                "*     * ",
-                "*     * ",
-                "*     * ",
-                "*       ");
-
-        banner[2] = String.join(" ",
-                "*     * ",
-                "*     * ",
-                "******  ",
-                " *****  ");
-
-        banner[3] = String.join(" ",
-                "*     * ",
-                "*     * ",
-                "*       ",
-                "      * ");
-
-        banner[4] = String.join(" ",
-                " *****  ",
-                " *****  ",
-                "*       ",
-                " *****  ");
-
-        for (String line : banner) {
-            System.out.println(line);
-        }
+        BannerPattern banner = new BannerPattern();
+        banner.display();
     }
 }
